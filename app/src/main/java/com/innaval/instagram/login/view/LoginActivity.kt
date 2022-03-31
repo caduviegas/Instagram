56c40330b0b4dca32a5962131877f89948720202
Eduardo <engenedu@hotmail.com>
2022-03-31 09:21:14 -0300

    First Commit

---
+++ app/src/main/java/com/innaval/instagram/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.innaval.instagram.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.innaval.instagram.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
